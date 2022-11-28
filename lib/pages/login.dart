bcc0f66a2dc8e3b31e8e4be8d4368f847509d615
Alez2312 <alejandropl2509@gmail.com>
2022-11-28 12:18:46 -0500

    v-12:18 - 28/11/22

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -39,105 +39,110 @@ class _LoginState extends State<Login> {
 
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+
     return Scaffold(
       backgroundColor: Colors.blue,
-      body: Container(
-        margin: const EdgeInsets.symmetric(vertical: 100, horizontal: 20),
-        width: 500,
-        height: 525.1,
-        decoration: BoxDecoration(
-            color: Colors.white,
-            border: Border.all(color: Colors.black, width: 2),
-            borderRadius: const BorderRadius.all(Radius.circular(15))),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Image.asset('assets/escudo.png', width: 100),
-                const SizedBox(width: 50.0),
-                Image.asset('assets/logo1.png', width: 250),
-              ],
-            ),
-            const Text(
-              'Control Patios',
-              style: TextStyle(
-                  fontSize: 32.0,
-                  color: Colors.grey,
-                  fontWeight: FontWeight.bold),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 30.0),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Form(
-                    key: _formKey,
-                    autovalidateMode: AutovalidateMode.onUserInteraction,
-                    child: Column(
-                      children: [
-                        TextFormField(
-                          controller: id,
-                          autocorrect: false,
-                          keyboardType: TextInputType.text,
-                          decoration: const InputDecoration(
-                            labelText: 'Ingrese número de documento',
-                            hintText: 'Número de documento',
-                            prefixIcon: Icon(Icons.account_circle_rounded),
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                        const SizedBox(height: 30.0),
-                        TextFormField(
-                          controller: pass1,
-                          keyboardType: TextInputType.visiblePassword,
-                          obscureText: contrasenaVisible,
-                          decoration: InputDecoration(
-                              labelText: 'Ingrese la contraseña',
-                              hintText: 'Contraseña',
-                              prefixIcon: const Icon(Icons.password_rounded),
-                              border: const OutlineInputBorder(),
-                              suffixIcon: IconButton(
-                                icon: Icon(contrasenaVisible
-                                    ? Icons.visibility
-                                    : Icons.visibility_off),
-                                onPressed: () {
-                                  setState(() {
-                                    contrasenaVisible = !contrasenaVisible;
-                                  });
-                                },
-                              )),
-                        ),
-                        const SizedBox(height: 30.0),
-                        MaterialButton(
-                            minWidth: double.infinity,
-                            onPressed: () {
-                              _login();
-                            },
-                            color: Colors.blue,
-                            textColor: Colors.white,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10)),
-                            //Remove?
-                            child: Container(
-                              margin: const EdgeInsets.all(15),
-                              child: const Text(
-                                'Ingresar',
-                                style: TextStyle(
-                                  fontSize: 20.0,
-                                ),
-                              ),
-                            )),
-                        const SizedBox(height: 6.8),
-                        TextButton(
-                            onPressed: () {},
-                            child: const Text(
-                              'Recuperar contraseña',
-                              style: TextStyle(fontSize: 20),
-                            ))
-                      ],
-                    )),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.symmetric(vertical: 100, horizontal: 20),
+          width: size.width * 1,
+          height: size.height * 0.67,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.black, width: 2),
+              borderRadius: const BorderRadius.all(Radius.circular(15))),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Image.asset('assets/escudo.png', width: size.width * 0.25),
+                  const SizedBox(width: 50.0),
+                  Image.asset('assets/logo1.png', width: size.width * 0.5),
+                ],
               ),
-            )
-          ],
+              const Text(
+                'Control Patios',
+                style: TextStyle(
+                    fontSize: 28.0,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.bold),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 15.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Form(
+                      key: _formKey,
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            controller: id,
+                            autocorrect: false,
+                            keyboardType: TextInputType.text,
+                            decoration: const InputDecoration(
+                              labelText: 'Ingrese número de documento',
+                              hintText: 'Número de documento',
+                              prefixIcon: Icon(Icons.account_circle_rounded),
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                          const SizedBox(height: 20.0),
+                          TextFormField(
+                            controller: pass1,
+                            keyboardType: TextInputType.visiblePassword,
+                            obscureText: contrasenaVisible,
+                            decoration: InputDecoration(
+                                labelText: 'Ingrese la contraseña',
+                                hintText: 'Contraseña',
+                                prefixIcon: const Icon(Icons.password_rounded),
+                                border: const OutlineInputBorder(),
+                                suffixIcon: IconButton(
+                                  icon: Icon(contrasenaVisible
+                                      ? Icons.visibility
+                                      : Icons.visibility_off),
+                                  onPressed: () {
+                                    setState(() {
+                                      contrasenaVisible = !contrasenaVisible;
+                                    });
+                                  },
+                                )),
+                          ),
+                          const SizedBox(height: 20.0),
+                          MaterialButton(
+                              minWidth: double.infinity,
+                              onPressed: () {
+                                _login();
+                              },
+                              color: Colors.blue,
+                              textColor: Colors.white,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10)),
+                              //Remove?
+                              child: Container(
+                                margin: const EdgeInsets.all(15),
+                                child: const Text(
+                                  'Ingresar',
+                                  style: TextStyle(
+                                    fontSize: 20.0,
+                                  ),
+                                ),
+                              )),
+                          const SizedBox(height: 10.8),
+                          TextButton(
+                              onPressed: () {},
+                              child: const Text(
+                                'Recuperar contraseña',
+                                style: TextStyle(fontSize: 20),
+                              ))
+                        ],
+                      )),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
